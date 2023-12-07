ad0335381ce9d21a109c077af5ec1d4e1bbc231d
Krosauer <kyle.rosauer@icloud.com>
2023-12-07 17:48:59 -0600

    Added the store item to increase perclick
    Function to make it work is still in the works in the javascript

--- clickerJS.js
+++ clickerJS.js
@@ -1,6 +1,29 @@
 var clicks = 0;
+var perClick = 1;
+var perClickPrice = {1:50,2:300,3:5000};//how much it costs per level for the per click powerup
+var perClickLevel = 1;
 
-function onClick() {
-  clicks += 1;
+
+function onClickMain() {
+  //add click increment to display
+  clicks += perClick;
   document.getElementById("clicks").innerHTML = clicks;
 };
+
+function onClickIncrease(){
+  //if you have enough money, increase level
+  //!!!TEMP!!! decrease total money dispay after buyinh 
+  if (document.getElementById("clicks").innerHTML > perClickPrice[perClickLevel]) {
+    perClickLevel += 1;
+  }
+}
+function increaseClick(level){
+  //based on what level you are on, perClick will increase
+  if (level === 1) {
+    perClick = 5;
+  }
+  if (level === 2) {
+    perClick = 10;
+  }
+
+}
